c1c50a8302ed617b9e70b10a6580b028d15f3896
Shun <Ys.cheng@goldenf.co>
2026-06-04 02:21:37 +0800

    feat: add Go backend for config and clipboard

--- app.go
+++ app.go
@@ -1,27 +1,76 @@
+// app.go
 package main
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
+	"os"
+	"path/filepath"
+
+	"github.com/atotto/clipboard"
 )
 
-// App struct
+type Config struct {
+	FavoriteIds []int  `json:"favoriteIds"`
+	LastLayout  string `json:"lastLayout"`
+}
+
+var defaultConfig = Config{
+	FavoriteIds: []int{0, 1, 2, 3, 11, 91, 92},
+	LastLayout:  "3,4,5,5,4,3",
+}
+
 type App struct {
 	ctx context.Context
 }
 
-// NewApp creates a new App application struct
 func NewApp() *App {
 	return &App{}
 }
 
-// startup is called when the app starts. The context is saved
-// so we can call the runtime methods
 func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
 }
 
-// Greet returns a greeting for the given name
-func (a *App) Greet(name string) string {
-	return fmt.Sprintf("Hello %s, It's show time!", name)
+func configPath() (string, error) {
+	dir, err := os.UserConfigDir()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(dir, "slot-board-utils", "config.json"), nil
+}
+
+func (a *App) GetConfig() Config {
+	path, err := configPath()
+	if err != nil {
+		return defaultConfig
+	}
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return defaultConfig
+	}
+	var cfg Config
+	if err := json.Unmarshal(data, &cfg); err != nil {
+		return defaultConfig
+	}
+	return cfg
+}
+
+func (a *App) SaveConfig(cfg Config) error {
+	path, err := configPath()
+	if err != nil {
+		return err
+	}
+	if err := os.MkdirAll(filepath.Dir(path), 0755); err != nil {
+		return err
+	}
+	data, err := json.MarshalIndent(cfg, "", "  ")
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(path, data, 0644)
+}
+
+func (a *App) CopyToClipboard(text string) error {
+	return clipboard.WriteAll(text)
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require github.com/wailsapp/wails/v2 v2.12.0
 
 require (
 	git.sr.ht/~jackmordaunt/go-toast/v2 v2.0.3 // indirect
+	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/bep/debounce v1.2.1 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
